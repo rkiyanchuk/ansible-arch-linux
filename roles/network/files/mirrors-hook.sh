9da36e76b1fbdf435b518f807ef4401169d1da74
Ruslan Kiianchuk <ruslan.kiianchuk@gmail.com>
2017-03-15 23:03:17 -0700

    Update network role

---
+++ roles/network/files/mirrors-hook.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Update pacman mirrors with reflector on new network connection.
+
+IF=$1
+STATUS=$2
+
+if [[ ${STATUS} == "up" ]]; then
+    reflector --sort score -p https --country {{ country }} --save /etc/pacman.d/mirrorlist
+fi
+
